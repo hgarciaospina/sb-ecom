31ede87024f0e23dd285ce9156c2e7061ff2e913
Henry García <henrygarciaospina@gmail.com>
2025-05-03 19:55:04 -0500

    Creation of Cart Models.

---
+++ src/main/java/com/ecommerce/project/model/Cart.java
@@ -0,0 +1,29 @@
+package com.ecommerce.project.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "carts")
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cart_id")
+    Long cartId;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "cart", cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE}, orphanRemoval = true)
+    private List<CartItem> cartItems = new ArrayList<>();
+
+    @Column(name = "total_price")
+    private Double totalPrice = 0.0;
+}
---
+++ src/main/java/com/ecommerce/project/model/CartItem.java
@@ -0,0 +1,33 @@
+package com.ecommerce.project.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "cart_items")
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cart_item_id")
+    private Long cartItemId;
+
+    @ManyToOne
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @Column(name = "quantity")
+    private Integer quantity;
+
+    @Column(name = "discount")
+    private Double discount;
+
+    @Column(name = "product_price")
+    private Double productPrice;
+}
