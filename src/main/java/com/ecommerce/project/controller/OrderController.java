e032eea400dea029e7c1ca9e083db1f9357f9b74
Henry García <henrygarciaospina@gmail.com>
2025-05-16 20:30:13 -0500

    Managing Order Placements

---
+++ src/main/java/com/ecommerce/project/controller/OrderController.java
@@ -0,0 +1,33 @@
+package com.ecommerce.project.controller;
+
+import com.ecommerce.project.payload.OrderDTO;
+import com.ecommerce.project.payload.OrderRequestDTO;
+import com.ecommerce.project.util.AuthUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    @Autowired
+    AuthUtil authUtil;
+    @PostMapping("/orders/users/payments/{paymentMethod}")
+    public ResponseEntity<OrderDTO> orderProducts(@PathVariable String paymentMethod,
+                                                   @RequestBody OrderRequestDTO orderRequestDTO) {
+        OrderDTO orderDTO = orderService.placeOrder(
+                orderRequestDTO.getAddressId(),
+                paymentMethod,
+                orderRequestDTO.getPgName(),
+                orderRequestDTO.getPgPaymentId(),
+                orderRequestDTO.getPgStatus(),
+                orderRequestDTO.getPgResponseMessage()
+        );
+        return new ResponseEntity<>(orderDTO, HttpStatus.CREATED);
+    }
+}
